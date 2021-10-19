52dd4197d75e9fce853f4c571d9a377c05785f72
lamouchiambre <37905543+lamouchiambre@users.noreply.github.com>
2021-10-19 14:56:23 -0400

    suite

--- utictactoe/src/utictactoe.c
+++ utictactoe/src/utictactoe.c
@@ -1,7 +1,9 @@
 #include "utictactoe.h"
+#include "../include/common.h"
+#include "../include/model.h"
 #include <stdio.h>
 int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
+
+   printf("Hello, World!\n");
    return 0;
 }
